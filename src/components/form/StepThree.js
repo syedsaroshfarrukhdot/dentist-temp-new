1a083794c515f68db41769038fc698de0e2550c4
syedsaroshfarrukhdot <81307212+syedsaroshfarrukhdot@users.noreply.github.com>
2021-05-04 17:08:27 +0500

    signin and signup added

--- src/components/form/StepThree.js
+++ src/components/form/StepThree.js
@@ -3,10 +3,27 @@ import { Button, ButtonGroup } from "react-bootstrap";
 import ProgressBar from "../progress-bar/ProgressBar";
 import Slider from "../slider/Slider";
 import house from "../../images/house-sq.svg";
-import formImageTwo from '../../images/tuev_500.png'
+import formImageTwo from "../../images/tuev_500.png";
 
+import { render } from "react-dom";
 
+// Import react-circular-progressbar module and styles
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
+// Animation
+import { easeQuadInOut } from "d3-ease";
+import AnimatedProgressProvider from "../animation-circle/AnimatedProgressProvider";
+import ChangingProgressProvider from "../animation-circle/ChangingProgressProvider";
+
+// Radial separators
+import RadialSeparators from "../animation-circle/RadialSeparators";
+
+const percentage = 66;
 
 const StepThree = ({ setForm, formData, navigation }) => {
   const { phone, email } = formData;
@@ -30,65 +47,108 @@ const StepThree = ({ setForm, formData, navigation }) => {
   };
 
   return (
-    <div className="form">
-      
-      <div className="progress_bar_container">
-        <div className="progress_align">
-        <p className="text_form">
-      Welche Fläche hat das Grundstück des Hauses?
-      </p>
-          <ProgressBar
-            color={"#085159"}
-            width={"750px"}
-            value={value}
-            max={100}
-          />
-        </div>
+    <>
+      <div className="animation">
+        <Example label="Sparpotential">
+          <AnimatedProgressProvider
+            valueStart={0}
+            valueEnd={58}
+            duration={1.4}
+            easingFunction={easeQuadInOut}
+          >
+            {(value) => {
+              const roundedValue = Math.round(value);
+              return (
+                <CircularProgressbarWithChildren
+                  value={value}
+                  strokeWidth={6}
+                  text={`${roundedValue}%`}
+                  /* This is important to include, because if you're fully managing the
+        animation yourself, you'll want to disable the CSS animation. */
+                  styles={buildStyles({
+                    pathTransition: "none",
+                    pathColor: "#59adb7",
+                    strokeLinecap: "butt",
+                    textColor: "black",
+                  })}
+                >
+                  <div style={{ fontSize: 25, marginTop: 25 }}>
+                    <small>
+                      für <span>30</span> jährige
+                    </small>
+                  </div>
+                </CircularProgressbarWithChildren>
+              );
+            }}
+          </AnimatedProgressProvider>
+        </Example>
+        <Example label="Tarifanzahl">
+          <AnimatedProgressProvider
+            valueStart={0}
+            valueEnd={79}
+            duration={1.4}
+            easingFunction={easeQuadInOut}
+          >
+            {(value) => {
+              const roundedValue = Math.round(value);
+              return (
+                <CircularProgressbarWithChildren
+                  value={value}
+                  strokeWidth={6}
+                  text={`${roundedValue}%`}
+                  /* This is important to include, because if you're fully managing the
+        animation yourself, you'll want to disable the CSS animation. */
+                  styles={buildStyles({
+                    pathTransition: "none",
+                    pathColor: "#59adb7",
+                    strokeLinecap: "butt",
+                    textColor: "black",
+                  })}
+                >
+                  <div
+                    style={{
+                      fontSize: 25,
+                      marginTop: 40,
+                      width: 75,
+                      lineHeight: "5px",
+                      textAlign: "center",
+                    }}
+                  >
+                    <small>Tarife wurden gefunden</small>
+                  </div>
+                </CircularProgressbarWithChildren>
+              );
+            }}
+          </AnimatedProgressProvider>
+        </Example>
       </div>
-
-      <div className="btn_container_slider">
-        <div className="Row_MultiStep">
-          <Slider
-            left_text="<200 m²"
-            right_text=">2500 m²"
-            value="1250"
-            min="200"
-            max="2500"
-            unit="m²"
-            text="Alternativ eintippen"
-            callBackFromParent={myCallBack}
-          />
-           <img className="img_slider_sasas" src={house} />
-          
-        </div>
-        <Button variant="danger" onClick={next} >Weiter ></Button>
+      <div className="btn_container_1_single">
+        <Button
+          className="animation_btn"
+          variant="primary"
+          onClick={next}
+          value="Weiter"
+        >
+          <b>></b>
+          <span className="glyphicon glyphicon-menu-right"></span> JETZT
+          VERGLEICHEN
+        </Button>
       </div>
-
-      <div className="btn_container_slider_mobile_slider">
-      <img className="img_slider_sasas" src={house} />
-      <div className="Row_MultiStep">
-          <Slider
-            left_text="<200 m²"
-            right_text=">2500 m²"
-            value="1250"
-            min="200"
-            max="2500"
-            unit="m²"
-            text="Alternativ eintippen"
-            callBackFromParent={myCallBack}
-          />
-          </div>
-        
-      </div>
-      <Button variant="danger2" onClick={next} >Weiter ></Button>
-
-      <img
-        src={formImageTwo}
-        alt="img"
-        className="imageCover"
-      />
-    </div>
+    </>
   );
 };
+
+function Example(props) {
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <div style={{ marginTop: 30 }}>
+        <div>
+          <h4 className="h5 aniimation-color">{props.label}</h4>
+        </div>
+        <div className="animation_size_one">{props.children}</div>
+      </div>
+    </div>
+  );
+}
 
 export default StepThree;
--- src/components/form/StepTwo.js
+++ src/components/form/StepTwo.js
@@ -11,21 +11,21 @@ import Styled from "styled-components";
 
 const Container = Styled.div`
 check-boc-custom{
-  background-color :${props => props.colorBg};
+  background-color :${(props) => props.colorBg};
 }  
 `;
 
-const StepTwo = ({ setForm, formData, navigation , colorBg }) => {
+const StepTwo = ({ setForm, formData, navigation, colorBg }) => {
   const { StepOne } = formData;
 
   const { previous, next } = navigation;
   const [value, setValue] = useState(12);
   const [state, setState] = useState(false);
   const [one, setOne] = useState(false);
-  const [color,setColor] = useState("grey")
-  const [color1,setColor1] = useState("grey")
-  const [bgcolor,setBGColor] = useState("white")
-  const [bgcolor1,setBGColor1] = useState("white")
+  const [color, setColor] = useState("grey");
+  const [color1, setColor1] = useState("grey");
+  const [bgcolor, setBGColor] = useState("white");
+  const [bgcolor1, setBGColor1] = useState("white");
 
   const myCallBack = (statefromchild) => {
     formData.StepSix = statefromchild;
@@ -47,40 +47,57 @@ const StepTwo = ({ setForm, formData, navigation , colorBg }) => {
           <Form.Row className="form-row-custom-checkbox">
             <Form.Group controlId="formGridFname" className="form-group-custom">
               <Container>
-              <div className="check-boc-custom" style={{backgroundColor:bgcolor}}>
-                <Checkbox
-                  name="one"
-                  checked={one}
-                  icon={<img src={tick} style={{ width: 24 , fill : "white" }} alt="" />}
-                  borderColor={color}
-                  borderRadius={4}
-                  size={24}
-                  label="weiblich"
-                  onChange={(checked) => {
-                    if (checked) {
-                      setOne(true);
-                      setState(false);
-                      setColor("#59ADB7");
-                      setBGColor("#59ADB7")
-                      setColor1("grey");
-                      setBGColor1("white")
+                <div
+                  className="check-boc-custom"
+                  style={{ backgroundColor: bgcolor }}
+                >
+                  <Checkbox
+                    name="one"
+                    checked={one}
+                    icon={
+                      <img
+                        src={tick}
+                        style={{ width: 24, fill: "white" }}
+                        alt=""
+                      />
                     }
-                    if(!checked){
-                      setColor("grey");
-                      setBGColor("white")
-                    }
-                  }}
-                  
-                />
-              </div>
-           </Container>
+                    borderColor={color}
+                    borderRadius={4}
+                    size={24}
+                    label="weiblich"
+                    onChange={(checked) => {
+                      if (checked) {
+                        setOne(true);
+                        setState(false);
+                        setColor("#59ADB7");
+                        setBGColor("#59ADB7");
+                        setColor1("grey");
+                        setBGColor1("white");
+                      }
+                      if (!checked) {
+                        setColor("grey");
+                        setBGColor("white");
+                      }
+                    }}
+                  />
+                </div>
+              </Container>
             </Form.Group>
             <Form.Group controlId="formGridFname" className="form-group-custom">
-              <div className="check-boc-custom" style={{backgroundColor:bgcolor1}}>
+              <div
+                className="check-boc-custom"
+                style={{ backgroundColor: bgcolor1 }}
+              >
                 <Checkbox
                   name="two"
                   checked={state}
-                  icon={<img src={tick} style={{ width: 24 , fill : "white"}} alt="" />}
+                  icon={
+                    <img
+                      src={tick}
+                      style={{ width: 24, fill: "white" }}
+                      alt=""
+                    />
+                  }
                   borderColor={color1}
                   borderRadius={4}
                   size={24}
@@ -90,13 +107,13 @@ const StepTwo = ({ setForm, formData, navigation , colorBg }) => {
                       setOne(false);
                       setState(true);
                       setColor1("#59ADB7");
-                      setBGColor1("#59ADB7")
+                      setBGColor1("#59ADB7");
                       setColor("grey");
-                      setBGColor("white")
+                      setBGColor("white");
                     }
-                    if(!checked){
+                    if (!checked) {
                       setColor1("grey");
-                      setBGColor1("white")
+                      setBGColor1("white");
                     }
                   }}
                 />
